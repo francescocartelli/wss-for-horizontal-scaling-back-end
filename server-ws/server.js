dee1d2018e0b8c64df934a1f3771bd815f82fdd9
francescocartelli <60184893+francescocartelli@users.noreply.github.com>
2024-04-29 21:08:56 +0200

    Added server ws for single connection assumption

---
+++ server-ws/server.js
@@ -0,0 +1,35 @@
+const WebSocket = require('ws')
+
+const { getPubSubClients } = require.main.require('./services/mq')
+const { getIdentity } = require.main.require('./services/identity')
+
+const boot = async () => {
+    const wss = new WebSocket.Server({ port: 8000 })
+
+    const [pubClient, subClient] = await getPubSubClients()
+
+    wss.on('connection', async (ws, req) => {
+        const userId = getIdentity(req)
+
+        subClient.subscribe(userId, message => {
+            ws.send(message)
+        })
+
+        ws.on('message', message => {
+            const { recipient, content } = JSON.parse(message)
+            const delivery = { recipient: recipient, sender: userId, content: content }
+
+            console.log(delivery)
+
+            pubClient.publish(recipient, JSON.stringify(delivery))
+        })
+
+        ws.on('close', () => {
+            subClient.unsubscribe(userId)
+        })
+    })
+
+    console.log(`WebSocket server running on port ${process.env.WSS_PORT}`)
+}
+
+boot()
